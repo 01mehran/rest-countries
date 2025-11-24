852d080a427671aaeff41f121fbd9c0596f7b852
mehran <mehranmohamadi1311@gmail.com>
2025-11-24 23:54:55 +0330

    chore: install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Rest Countries</h1>
+      <h1 className="text-center bg-red-600 text-4xl text-yellow-400 p-4">Rest Countries</h1>
     </>
   );
 }
 
 export default App;
-// https://github.com/01mehran/rest-countries.git
