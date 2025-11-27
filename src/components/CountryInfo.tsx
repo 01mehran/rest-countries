b5f386557467c0730c81c3653202efbff1e5eb96
mehran <mehranmohamadi1311@gmail.com>
2025-11-28 03:23:08 +0330

    style: fix countryInfo height/width & improve responsive design

--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -6,17 +6,21 @@ interface CountryInfoProps {
 
 function CountryInfo({ data }: CountryInfoProps) {
   return (
-    <div className="bg-text-dark dark:bg-element-dark dark:text-bg-light/90 dark:shadow-bg-dark mx-auto h-full cursor-pointer rounded-md shadow-sm transition-transform duration-200 hover:scale-105 hover:shadow-md sm:w-62 md:w-[22%] dark:shadow-xl">
-      <article>
-        <img src={data.flag} alt="country flag" className="rounded-t-md" />
+    <div className="bg-text-dark dark:bg-element-dark dark:text-bg-light/90 dark:shadow-bg-dark h-80 w-full cursor-pointer rounded-md shadow-sm transition-transform duration-200 hover:scale-105 hover:shadow-md dark:shadow-xl">
+      <article className="h-40">
+        <img
+          src={data.flag}
+          alt="country flag"
+          className="h-full w-full rounded-t-md object-cover"
+        />
       </article>
-      <article className="p-4">
+      <article className="p-5">
         <p className="mb-3 font-bold">{data.name}</p>
-        <div className="flex flex-col space-y-px">
+        <div className="flex flex-col space-y-px leading-6">
           <p className="text-[13px] font-medium">
             Population:{' '}
             <span className="dark:text-bg-light/75 font-light">
-              {data.population}
+              {data.population.toLocaleString()}
             </span>
           </p>
           <p className="text-[13px] font-medium">
