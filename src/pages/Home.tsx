6008ab7502a7094462fabc343d5b8aa0d03b10d4
mehran <mehranmohamadi1311@gmail.com>
2025-12-02 01:48:28 +0330

    feat: add search country logic by name. Closes #1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,9 +8,41 @@ import { MdSearch } from 'react-icons/md';
 
 // Services;
 import GetAllCountries from '@/servises/GetAllCountries';
+import { useEffect, useState } from 'react';
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
   const { allCountries, isLoading, isError } = GetAllCountries();
+  const [searchedCountries, setSearchedCountries] = useState(allCountries);
+  const [msg, setMsg] = useState<string | null>('');
+
+  useEffect(() => {
+    setSearchedCountries(allCountries);
+  }, [allCountries]);
+
+  const handleSearchCountry = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const value = e.target.value;
+    setSearchQuery(value);
+
+    if (value.trim() === '') {
+      setSearchedCountries(allCountries);
+      setMsg('');
+      return;
+    }
+    const searchedCountries = allCountries.filter((c) =>
+      c.name.common
+        .toLocaleLowerCase()
+        .includes(searchQuery?.toLocaleLowerCase()),
+    );
+    if (!searchedCountries.length) {
+      setMsg('No country found');
+    } else {
+      setMsg('');
+    }
+    setSearchedCountries(searchedCountries);
+  };
 
   return (
     <div className="bg-bg-light dark:bg-bg-dark min-h-dvh pb-4">
@@ -18,14 +50,15 @@ function Home() {
       <main className="px-4 lg:px-24">
         {/* Search input */}
         <section className="mt-8 items-center space-y-6 sm:justify-between md:flex md:space-y-0">
-          <article className="bg-text-dark dark:bg-element-dark relative mx-auto h-12 w-full rounded-md shadow-sm sm:mx-0 sm:max-w-[400px]">
+          <form className="bg-text-dark dark:bg-element-dark relative mx-auto h-12 w-full rounded-md shadow-sm sm:mx-0 sm:max-w-[400px]">
             <input
               type="text"
               placeholder="Search for a country..."
               className="placeholder:text-input-light dark:text-text-dark dark:placeholder:text-text-dark h-full w-full rounded-md border-0 pr-4 pl-18 tracking-tight outline-0 placeholder:text-[14px] placeholder:font-medium"
+              onChange={handleSearchCountry}
             />
             <MdSearch className="text-input-light dark:text-text-dark absolute top-1/2 translate-7 -translate-y-1/2 transform text-2xl" />
-          </article>
+          </form>
           {/* Filter by region */}
           <article className="bg-text-dark dark:bg-element-dark w-42 rounded-md px-1.5 shadow-sm">
             <select className="text-element-dark dark:text-text-dark dark:bg-element-dark flex h-12 w-full items-center rounded-md border-0 pr-4 text-2xl text-[14px] font-semibold outline-0">
@@ -46,10 +79,16 @@ function Home() {
             </p>
           )}
 
+          {msg && (
+            <p className="font-bold text-red-600 text-shadow-md text-shadow-slate-300 dark:text-shadow-black">
+              {msg}
+            </p>
+          )}
+
           {isLoading ? (
             <Loading />
           ) : (
-            allCountries.map((country) => (
+            searchedCountries.map((country) => (
               <CountryInfo key={country.id} countryData={country} />
             ))
           )}
