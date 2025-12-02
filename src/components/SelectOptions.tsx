abfee9f51831318a74af1867bb5e04833852e5b7
mehran <mehranmohamadi1311@gmail.com>
2025-12-02 23:22:36 +0330

    fix: select/options values & text size

--- src/components/SelectOptions.tsx
+++ src/components/SelectOptions.tsx
@@ -26,9 +26,9 @@ function SelectOptions() {
         onClick={handleOpenOptions}
         className="bg-text-dark dark:bg-element-dark flex w-full cursor-pointer items-center justify-between rounded-md px-4 py-3 shadow-sm"
       >
-        <p className="text-md">{selectedRegion || 'Filter by Region'}</p>
+        <p className="text-sm">{selectedRegion || 'Filter by Region'}</p>
         <span
-          className={`text-2xl ${isRegionListOpen ? 'rotate-180' : ''} transition-transform`}
+          className={`text-2xl ${isRegionListOpen ? 'rotate-180' : ''} transition-transform duration-300`}
         >
           <MdKeyboardArrowDown />
         </span>
@@ -36,28 +36,28 @@ function SelectOptions() {
 
       {/* Regions list; */}
       <Activity mode={isRegionListOpen ? 'visible' : 'hidden'}>
-        <ul className="text-element-dark dark:text-text-dark dark:bg-element-dark bg-text-dark absolute -bottom-45 z-10 w-full cursor-pointer items-center space-y-2 rounded-md border-0 px-5 py-3 font-semibold shadow-sm outline-0">
+        <ul className="text-element-dark dark:text-text-dark dark:bg-element-dark bg-text-dark absolute -bottom-45 z-10 w-full cursor-pointer items-center space-y-3 rounded-md border-0 px-5 py-3 text-sm font-semibold shadow-sm outline-0">
           <li
             className="transition-px duration-200 hover:px-1.5"
-            onClick={() => handleSelectedRegion('Asia')}
+            onClick={() => handleSelectedRegion('Africa')}
           >
             Africa
           </li>
           <li
             className="transition-px duration-200 hover:px-1.5"
-            onClick={() => handleSelectedRegion('Europe')}
+            onClick={() => handleSelectedRegion('America')}
           >
             America
           </li>
           <li
             className="transition-px duration-200 hover:px-1.5"
-            onClick={() => handleSelectedRegion('America')}
+            onClick={() => handleSelectedRegion('Asia')}
           >
             Asia
           </li>
           <li
             className="transition-px duration-200 hover:px-1.5"
-            onClick={() => handleSelectedRegion('Africa')}
+            onClick={() => handleSelectedRegion('Europe')}
           >
             Europe
           </li>
